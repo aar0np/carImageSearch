a4355c61753362ecdfa92702d3fe9d67a1702920
Aaron Ploetz <aaron.ploetz@aarons-mbp.mynetworksettings.com>
2024-01-18 13:17:37 -0600

    added CarSearch.com banner

--- carSearch.py
+++ carSearch.py
@@ -3,9 +3,10 @@ import justpy as jp
 from carServices import get_car_by_text
 
 webp = jp.WebPage()
+jp.Img(src="/static/web_images/carsearch_logo.png", a=webp)
 input1 = jp.InputChangeOnly(placeholder="enter search text",a=webp)
 input1.div = jp.Div(text="Last search appears here",a=webp)
-image = jp.Img(classes='m-2 p-2', a=webp)
+image = jp.Img(classes='m-2 p-2', a=webp, width=600)
 
 async def search_handler(self, msg):
 	self.div.text = self.value
